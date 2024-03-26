598f491187e1245031fd24311ab34ee6b93b3b60
carl hedgren <carl.hedgren@nav.no>
2024-03-26 09:48:26 +0100

    cute comments

--- one/test.nix
+++ one/test.nix
@@ -1,12 +1,16 @@
 {
+  # The name of the test
   name = "Let's have several machines";
 
+  # We can have many nodes
   nodes = {
+    # These attrs are hostnames
     machine1 = { pkgs, ... }: { };
     machine2 = { pkgs, ... }: { };
   };
 
   testScript = ''
+    # The hostnames above exist in the hostfiles on the machines, conveniently
     machine1.wait_for_unit("network-online.target")
     machine2.wait_for_unit("network-online.target")
 
--- zero/test.nix
+++ zero/test.nix
@@ -1,11 +1,15 @@
 {
+  # The Name of the test
   name = "we can hello";
 
+  # the machines in the test: none, one, or many
   nodes = {
+    # This is a hostname
     myCoolTestMachine = { pkgs, ... }: { };
 
   };
 
+  # The test script, in python
   testScript = ''
     myCoolTestMachine.succeed("exit 1")
   '';
