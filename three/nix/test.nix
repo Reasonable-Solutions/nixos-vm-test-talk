f43c3dbc5b8501fc1cd39e35b4f8669dbc09ba3f
carl hedgren <carl.hedgren@nav.no>
2024-03-26 14:13:32 +0100

    fail: libressl not found que?

--- three/nix/test.nix
+++ three/nix/test.nix
@@ -1,22 +1,33 @@
 {
-  name = "Let's have several machines II";
+  name = "Do something with containers";
 
-  nodes = {
-    server = { pkgs, config, ... }: {
-      imports = [ ./module.nix ];
-      services.echo.enable = true;
-      networking.firewall.allowedTCPPorts = [ config.services.echo.port ];
+  nodes = let k3secret = "foo";
+  in {
+    k3I = { pkgs, ... }: {
+
+      networking.firewall.allowedTCPPorts = [
+        6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+        # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+        # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+      ];
+      networking.firewall.allowedUDPPorts = [
+        # 8472 # k3s, flannel: required if using multi-node for inter-node networking
+      ];
+      services.k3s.enable = true;
+      services.k3s.role = "server";
+      services.k3s.extraFlags = toString [
+        # "--kubelet-arg=v=4" # Optionally add additional args to k3s
+      ];
+      environment.systemPackages = [ pkgs.k3s ];
+
     };
+
     client = { pkgs, ... }: {
       environment.systemPackages = with pkgs; [ curl ];
-
     };
   };
 
   testScript = ''
-    server.wait_for_unit("network-online.target")
-    client.wait_for_unit("network-online.target")
-    _, output = client.execute("curl server:8090/hello")
-    assert output == "hello\n"
+    k3III.wait_for_unit("k3s.target")
   '';
 }
